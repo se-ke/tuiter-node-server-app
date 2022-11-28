7eda28180d5bad250907790c340260020fe38e05
Sean Kelly <skboogi@live.com>
2022-11-28 18:14:50 -0500

    Update schema with missing fields

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,13 @@ const schema = new Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    image: String,
+    handle: String,
+    time: String,
+    username: String,
+    replies: Number,
+    retuits: Number,
+    disliked: Boolean,
+    dislikes: Number
 }, {collection: 'tuits'});
 export default schema;
